48d58c1e659c8dc13aef8099329f6a0b7237cad3
Deyvid Santos <deyvidsantosdatascience@gmail.com>
2024-02-05 16:43:27 -0300

    Errors in hapi using hapi boom

--- buy-and-sell-backend/src/routes/getListing.js
+++ buy-and-sell-backend/src/routes/getListing.js
@@ -1,3 +1,4 @@
+import Boom from '@hapi/boom';
 import { fakeListings } from "./fake-data";
 
 export const getListingRoute = {
@@ -5,6 +6,8 @@ export const getListingRoute = {
     path: '/api/listings/{id}',
     handler: (req, h) => {
         const id = req.params.id;
-        return fakeListings.find(listings => listings.id === id);
+        const listing = fakeListings.find(listings => listings.id === id);
+        if (!listing) throw Boom.notFound(`Listing does not exist with ${id}`);
+        return listing;
     }
 }
