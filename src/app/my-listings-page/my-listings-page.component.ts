1fa6a948c225e53e8f581a222d184aeeb750250e
Deyvid Santos <deyvidsantosdatascience@gmail.com>
2024-02-05 12:42:20 -0300

    Create a My Listings page

--- src/app/my-listings-page/my-listings-page.component.ts
+++ src/app/my-listings-page/my-listings-page.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Listing } from "../types";
+import { fakeMyListings } from "../fake-data";
 
 @Component({
-  selector: 'app-my-listings-page',
-  templateUrl: './my-listings-page.component.html',
-  styleUrl: './my-listings-page.component.css'
+  selector: "app-my-listings-page",
+  templateUrl: "./my-listings-page.component.html",
+  styleUrl: "./my-listings-page.component.css",
 })
-export class MyListingsPageComponent {
+export class MyListingsPageComponent implements OnInit {
+  listings: Listing[] = [];
 
+  constructor() {}
+
+  ngOnInit(): void {
+    this.listings = fakeMyListings;
+  }
+
+  onDeleteClicked(listingId: string): void {
+    alert(`Deleting your listing with ${listingId}`);
+  }
 }
