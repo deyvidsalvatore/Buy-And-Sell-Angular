8ce8834f7152622339c484712039dd69945ec5e6
Deyvid Santos <deyvidsantosdatascience@gmail.com>
2024-02-05 12:50:40 -0300

    Create a New Listings page

--- src/app/new-listing-page/new-listing-page.component.ts
+++ src/app/new-listing-page/new-listing-page.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-new-listing-page',
   templateUrl: './new-listing-page.component.html',
   styleUrl: './new-listing-page.component.css'
 })
-export class NewListingPageComponent {
+export class NewListingPageComponent implements OnInit {
+  
+  name: string = '';
+  description: string = '';
+  price: string = '';
 
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    
+  }
+
+  onSubmit(): void {
+    alert('Creating a new listing...');
+    this.router.navigateByUrl('/my-listings')
+  }
 }
