5d8eda4e0115f2df24e78aa67a8ca308efc739f9
Deyvid Santos <deyvidsantosdatascience@gmail.com>
2024-02-05 16:17:00 -0300

    Set up a hapi server

---
+++ buy-and-sell-backend/src/server.js
@@ -0,0 +1,25 @@
+import Hapi from '@hapi/hapi';
+
+const start = async () => {
+    const server = Hapi.server({
+        port: 8000,
+        host: 'localhost'
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/hello',
+        handler: (req, h) => {
+            return 'Hello Hapi!';
+        }
+    });
+    await server.start();
+    console.log(`Server is listening on ${server.info.uri}`);
+}
+
+process.on('unhandledRejection', err => {
+    console.log(err);
+    process.exit(1);
+});
+
+start();
