3f3349fe08846cf2ad3cba9bdc234e763769597a
Deyvid Santos <deyvidsantosdatascience@gmail.com>
2024-02-07 17:16:09 -0300

    Create an endpoint to get a user listing

--- src/app/fake-data.ts
+++ src/app/fake-data.ts
@@ -5,31 +5,41 @@ export const fakeListings: Listing[] = [{
     name: 'Old Boat',
     description: 'A very old boat. Bargain price',
     price: 700,
+    views: 0,
 }, {
     id: '345',
     name: 'Computer',
     description: 'From the 1990s, a classic!',
     price: 50,
+    views: 0,
 }, {
     id: '456',
     name: 'Basketball Hoop',
     description: 'Good condition, free delivery',
     price: 100,
+    views: 0,
 }];
 
-export const fakeMyListings: Listing[] = [{
-    id: '789',
-    name: 'Bicycle',
-    description: 'Bright blue, very fast',
+export const fakeMyListings: Listing[] = [
+  {
+    id: "789",
+    name: "Bicycle",
+    description: "Bright blue, very fast",
     price: 75,
-}, {
-    id: '234',
-    name: 'Electric Guitar',
-    description: 'Needs new strings, but otherwise in very good condition',
+    views: 0,
+  },
+  {
+    id: "234",
+    name: "Electric Guitar",
+    description: "Needs new strings, but otherwise in very good condition",
     price: 99,
-}, {
-    id: '987',
-    name: 'Playstation',
-    description: 'Old playstation, still works though',
+    views: 0,
+  },
+  {
+    id: "987",
+    name: "Playstation",
+    description: "Old playstation, still works though",
     price: 75,
-}];
+    views: 0,
+  },
+];
