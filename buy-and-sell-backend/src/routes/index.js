8d7efb9a492be35fe64e4125ae9389364a8b0641
Deyvid Santos <deyvidsantosdatascience@gmail.com>
2024-02-07 17:37:57 -0300

    Create an endpoint to edit listings

--- buy-and-sell-backend/src/routes/index.js
+++ buy-and-sell-backend/src/routes/index.js
@@ -3,11 +3,13 @@ import { createNewListingRoute } from "./createNewListing";
 import { getAllListingsRoute } from "./getAllListings";
 import { getListingRoute } from "./getListing";
 import { getUserListingsRoute } from "./getUserListingsRoute";
+import { updateListingRoute } from "./updateListingRoute";
 
 export default [
     addViewToListingRoute,
     getAllListingsRoute,
     getListingRoute,
     getUserListingsRoute,
-    createNewListingRoute
+    createNewListingRoute,
+    updateListingRoute
 ];
