2f22c34dd6e1f873a4b6f7e1fe1f704dcf7664df
Deyvid Santos <deyvidsantosdatascience@gmail.com>
2024-02-07 16:55:35 -0300

    Connect to a MySQL database from node

--- buy-and-sell-backend/src/server.js
+++ buy-and-sell-backend/src/server.js
@@ -1,6 +1,8 @@
 import Hapi from '@hapi/hapi';
 import routes from './routes';
+import { db } from './database'
 
+let server; 
 const start = async () => {
     const server = Hapi.server({
         port: 8000,
@@ -8,7 +10,9 @@ const start = async () => {
     });
 
     routes.forEach(route => server.route(route));
-    
+
+    db.connect();
+
     await server.start();
     console.log(`Server is listening on ${server.info.uri}`);
 }
@@ -18,4 +22,11 @@ process.on('unhandledRejection', err => {
     process.exit(1);
 });
 
+process.on('SIGINT', async () => {
+    console.log('Stopping server...');
+    await server.stop({timeout: 1000});
+    db.end();
+    console.log('Server stopped!');
+    process.exit(0);
+})
 start();
